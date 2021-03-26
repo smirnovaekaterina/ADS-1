ebe460afaa9967c41f39c63702fca4db6b366343
smirnovaekaterina <75855500+smirnovaekaterina@users.noreply.github.com>
2021-03-26 11:49:24 +0300

    fix cpplink

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,27 +1,5 @@
 // Copyright 2021 NNTU-CS
-
-//int cbinsearch(int* arr, int size, int value) {
-//  int i = 0, j = size - 1, c = 0;
-//  while (i < j) {
-//    int m = (i + j) / 2;
-//    if (arr[m] < value)
-//      i = m + 1;
-//    else
-//      j = m;
-//  }
-//
-//  if (arr[i] == value) {
-//    while (arr[j] == value) {
-//      c++;
-//      i++;
-//    }
-//  }
-//
-//  if (c)
-//    return c;
-//  else
-//    return 0; // если ничего не найдено
-//}
+#include "alg.h"
 
 int cbinsearch(int* arr, int size, int value) {
   int count = 0;
